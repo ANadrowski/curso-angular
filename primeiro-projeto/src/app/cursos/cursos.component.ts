25f1c68cefe30c6f382fb0e066077a1f65aa650e
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2023-06-17 18:43:24 -0300

    Criando um servico usando injeção de dependencias

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -8,12 +9,11 @@ import { Component } from '@angular/core';
 export class CursosComponent {
 
   nomePortal: string;
-
   cursos: string[];
 
-  constructor() {
+  constructor(private cursosService: CursosService) {
     this.nomePortal = "http://loiane.training";
-    this.cursos = ['Java', 'Ext JS', 'Angular'];
+    this.cursos = cursosService.getCursos();
   }
 
 }
