400249b5f2b3f95089d0ddd0b6ac2c9e6e9564f0
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2023-07-08 15:29:06 -0300

    Começando com Pipes

---
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.scss']
+})
+export class ExemplosPipesComponent {
+
+  livro: any = {
+    titulo: 'Lerning Java',
+    rating: '4.54321',
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'http://www.google.com'
+  }
+
+}
