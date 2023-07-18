095dde2590ee6c7695967bd3f21cbde73795bc66
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2023-07-18 09:07:49 -0300

    Inicializando valores com o ngModel usando two-way databinding

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -7,13 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario: any = {
+    nome: 'Loiane',
+    email: 'loiane@email.com'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onSubmit(form: any) {
-    console.log(form);
+    console.log(form.value);
+
+    console.log(this.usuario);
   }
 
 }
