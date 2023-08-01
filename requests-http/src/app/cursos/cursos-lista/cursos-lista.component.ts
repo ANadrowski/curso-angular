e15091c95b04d964df9d985d51435303ca8b1548
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2023-07-31 21:57:03 -0300

    http get: listar registros

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CursosService } from '../cursos.service';
+import { Observable } from 'rxjs';
+import { Curso } from '../curso';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./cursos-lista.component.scss']
 })
 export class CursosListaComponent {
+
+  cursos: Curso[];
+  //cursos$: Observable<Curso[]>;
+
+  constructor(private cursosService : CursosService) {
+    cursos = [ ''];
+
+    cursosService.list().subscribe(
+      dados => this.cursos = dados
+    );
+  }
 
 }
